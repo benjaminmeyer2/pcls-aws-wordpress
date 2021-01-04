4089a20bc255885d0da1e8ab9440fcd5ab8a5cf0
benjaminmeyer2 <47785385+benjaminmeyer2@users.noreply.github.com>
2021-01-04 11:17:51 +0100

    Update footer.php

--- wp-content/themes/twentyseventeen/footer.php
+++ wp-content/themes/twentyseventeen/footer.php
@@ -18,6 +18,7 @@
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="wrap">
+				<p>yourrandomemail@gmail.com</p>
 				<?php
 				get_template_part( 'template-parts/footer/footer', 'widgets' );
 
